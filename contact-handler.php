a777556a44405ff79755f20b1c071119003080df
Charles <44501972+charlesw25@users.noreply.github.com>
2020-08-23 10:53:10 -0400

    Small update to timeline.

--- contact-handler.php
+++ contact-handler.php
@@ -9,14 +9,6 @@ if (isset($_POST['submit'])) {
     $headers = "Message from: " .$email_from;
     $txt = "Someone from charlesw25.github.io sent you a message.\n\n" .$message;
     $send = mail($email_to, $subject, $txt, $headers);
-
-    if ($send) {
-        echo '<br>';
-        echo 'Thanks for reaching out. I will get back to you shortly.';
-    } else {
-        echo 'Error. Make sure all forms are properly filled out and try again later.';
-    }
-
     header("Location: charlesw25.github.io");
 }
 ?>
