3d9ec8f0635c9e93c84c5db4b6cfcbf5709b3ac7
Charles Wood <woodcharles544@email.com>
2024-12-20 14:24:58 -0500

    init2

---
+++ portfolio/portfolio/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+
+function Links() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="https://drive.google.com/file/d/1JTK7vjSLoYMagUvfV8HkdY-GkukEp8Fi/view" target="_blank" rel="noopener noreferrer">Resume</a>
+        </li>
+        <li>
+          <a href="https://github.com/charlesw25" target="_blank" rel="noopener noreferrer">GitHub</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+function App() {
+  document.body.style.backgroundColor = "#F5F5F5"
+  return (
+    <div className="App">
+      <header className="App-header inter-main">
+        <h1>
+          charles wood
+        </h1>
+        <h4>Software Engineer</h4>
+        <Links />
+      </header>
+    </div>
+  );
+}
+
+export default App;
